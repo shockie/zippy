fb479dfffa0507ec8c09625b55e80018993848cc
Jordy <jordyvangelder@gmail.com>
2011-01-25 10:21:09 +0100

    added event machine

---
+++ mvc/event.js
@@ -0,0 +1,37 @@
+(function(){
+	function Event(){
+		this._listeners = {};
+	}
+	
+	Event.prototype.on = function(event, handler){
+		if(!this._listeners){
+			this._listeners = {};
+		}
+		if(!this._listeners[event]){
+			this._listeners[event] = [];
+		}
+		this._listeners[event].push(handler);
+	}
+	
+	Event.prototype.fire = function(event, data){
+		if(!(event||this._listeners[event])){
+			return;
+		}
+		for(var i=0; i < this._listeners[event].length; i++){
+			this._listeners[event][i](data);
+		}
+	}
+	
+	Event.prototype.unregister = function(event, cb){
+		if(!(cb||event||this._listeners[event])){
+			return;
+		}
+		var stringified = cb.toString();
+		for(var i=0; i<this._listeners[event].length; i++){
+			if(this._listeners[event][i].toString() === stringified){
+				this._listeners[event].splice(i,1);
+			}
+		}
+	}
+	this.Event = Event;
+}).call(Zippy);
